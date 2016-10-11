ebda18fec2fec325acd65a18fe317b5e2d5a39c6
gerbenvandervries <gerbenvandervries@gmail.com>
2016-10-11 14:21:58 +0200

    .

--- test/test_pipeline.sh
+++ test/test_pipeline.sh
@@ -63,6 +63,7 @@ sh submit.sh
 cd ${workfolder}/projects/PlatinumSubset_NGS_RNA/run01/jobs/
 perl -pi -e 's|--emitRefConfidence|-L 1:1-1200000 \\\n  --emitRefConfidence|' s*_GatkHaplotypeCallerGvcf_0.sh
 perl -pi -e 's|-stand_emit_conf|-L 1:1-1200000 \\\n  -stand_emit_conf|' s*_GatkGenotypeGvcf_*.sh
+perl -pe 's|GC.png|GC.png\n touch /groups/umcg-gaf//tmp04/tmp//PlatinumSubset_NGS_RNA/run01//PlatinumSample.GC.png|' s*_FastQC_*.sh
 perl -pi -e 's|--time=16:00:00|--time=05:59:00|' *.sh
 perl -pi -e 's|--time=23:59:00|--time=05:59:00|' *.sh
 
