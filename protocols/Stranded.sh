995d5962da5d252bd47291ad6e5757ba6e1ebcf3
kdelange <kimatwork01@gmail.com>
2020-07-02 16:44:13 +0200

    updated Stranded.sh

--- protocols/Stranded.sh
+++ protocols/Stranded.sh
@@ -1,5 +1,6 @@
 #MOLGENIS nodes=1 ppn=1 mem=10gb walltime=05:00:00
 
+#Parameter mapping
 #string.RSeQCVersion
 #string alignedFilteredBam
 #string sortedBam
@@ -8,7 +9,7 @@
 #string logsDir
 #string intermediateDir
 
-
+#Load module
 module load "${RSeQCVersion}"
 module list
 
@@ -18,16 +19,11 @@ tmpintermediateDir=${MC_tmpFile}
 
 echo "## "$(date)" Start $0"
 
-# for i in $(ls "${intermediateDir}"/*.Aligned.sortedByCoord.out.bam -1 |shuf -n 1)
-# do 
-# 	infer_experiment.py -r ${bed} -i "${i}"
-# done > "{tmpintermediateDir}"/"${i}".stranded.txt
-
-
 for i in $(ls "${intermediateDir}"/*.Aligned.sortedByCoord.out.bam -1 |shuf -n 1)
-do
-	infer_experiment.py -i "${i}"
+do 
+	infer_experiment.py -r /apps/data/Ensembl/GrCh37.75/All_Exon/Ensembl.GRCh37.75-AllExons_+20_-20bp.bed -i "${i}"
 done > "{tmpintermediateDir}"/"${i}".stranded.txt
+
 
 mv "{tmpintermediateDir}"/"${i}".stranded.txt "${intermediateDir}"
 
