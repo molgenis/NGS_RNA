348b1f653639b1b8d2826a6dbd765c502eed2f54
gerbenvandervries <gerbenvandervries@gmail.com>
2022-12-21 10:07:56 +0000

    fix paths

--- test/protocols/Autotest.sh
+++ test/protocols/Autotest.sh
@@ -16,13 +16,13 @@ module load ngs-utils
 
 cmp --silent "${testResults}/PlatinumSubset_NGS_RNA.expression.counts.table" "${projectResultsDir}/expression/PlatinumSubset_NGS_RNA.expression.counts.table" || echo "there are differences in expression between the test and the original output" > "${testResults}/output_NGS_RNA/expression.fail"
 
-cmp --silent "${testResults}/PlatinumSubset_NGS_RNA_deseq2_control_vs_sample.csv" "${projectResultsDir}/PlatinumSubset_NGS_RNA_deseq2_control_vs_sample.csv" || echo "files are different."  > "${testResults}/output_NGS_RNA/deseq2.fail"
+cmp --silent "${testResults}/PlatinumSubset_NGS_RNA_deseq2_control_vs_sample.csv" "${projectResultsDir}/expression/deseq2/PlatinumSubset_NGS_RNA_deseq2_control_vs_sample.csv" || echo "files are different."  > "${testResults}/output_NGS_RNA/deseq2.fail"
 
 for sample in SRR15529062 SRR15529064 SRR15529069
 do
-	cmp --silent "${testResults}/${sample}.leafcutter.report.tsv" "${projectResultsDir}/${sample}.leafcutter.report.tsv" || echo "Leafcutter failed" > "${testResults}/output_NGS_RNA/leafcutter.fail"
-	cmp --silent "${testResults}/${sample}.rMATS.format.tsv" "${projectResultsDir}/${sample}.rMATS.format.tsv" || echo "RMats failed" > "${testResults}/output_NGS_RNA/RMats.fail"
-	cmp --silent "${testResults}/${sample}.SJ.filtered.annotated.tsv" "${projectResultsDir}/${sample}.SJ.filtered.annotated.tsv" || echo "STAR failed" > "${testResults}/output_NGS_RNA/STAR.fail"
+	cmp --silent "${testResults}/${sample}.leafcutter.report.tsv" "${projectResultsDir}/leafcutter/${sample}.leafcutter.report.tsv" || echo "Leafcutter failed" > "${testResults}/output_NGS_RNA/leafcutter.fail"
+	cmp --silent "${testResults}/${sample}.rMATS.format.tsv" "${projectResultsDir}/rmats/${sample}/${sample}.rMATS.format.tsv" || echo "RMats failed" > "${testResults}/output_NGS_RNA/RMats.fail"
+	cmp --silent "${testResults}/${sample}.SJ.filtered.annotated.tsv" "${projectResultsDir}/star_sj/${sample}.SJ.filtered.annotated.tsv" || echo "STAR failed" > "${testResults}/output_NGS_RNA/STAR.fail"
 done
 
 if [[ -f "${testResults}/output_NGS_RNA/notInVcf1.txt" || -f "${testResults}/output_NGS_RNA/notInVcf2.txt" || -f "${testResults}/output_NGS_RNA/inconsistent.txt" || "${testResults}/output_NGS_RNA/"*.fail ]]
