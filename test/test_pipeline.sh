865a058cf56520d0a35077661157339b9c6a8957
gerbenvandervries <gerbenvandervries@gmail.com>
2026-05-06 13:15:09 +0200

    wip

--- test/test_pipeline.sh
+++ test/test_pipeline.sh
@@ -17,9 +17,11 @@ OUTDIR=""
 LOG_PREFIX="[TEST_PIPELINE]"
 ERROR_PREFIX="[ERROR]"
 TMP='tmp08'
+GROUP='umcg-atd'
+
 
 # ========================
-# HELP
+# FUNCTIONS
 # ========================
 print_help() {
 cat <<EOF
@@ -42,15 +44,44 @@ Example:
 EOF
 }
 
-# ========================
-# LOGGING
-# ========================
 log() {
   echo -e "$(date '+%F %T') ${LOG_PREFIX} $*"
 }
 die() {
   echo -e "$(date '+%F %T') ${ERROR_PREFIX} $*"
 }
+
+function checkIfFinished(){
+	local _projectName="${1}"
+	count=0
+	minutes=0
+	while [ ! -f "/groups/${GROUP}/${TMP}/projects/NGS_RNA/${_projectName}/run01/jobs/*CopyToResultsDir_0.sh.finished" ]
+	do
+
+		echo "${_projectName} is not finished in $minutes minutes, sleeping for 2 minutes"
+		sleep 120
+		minutes=$((minutes+2))
+
+		count=$((count+2))
+		if [[ "${count}" -eq 35 ]]
+		then
+			echo "the test was not finished within 35 minutes, let's kill it"
+			echo -e "\n"
+			for i in "/groups/${GROUP}/${TMP}/projects/NGS_RNA/${_projectName}/run01/jobs/"*".sh"
+			do
+				if [[ ! -f "${i}.finished" ]]
+				then
+					echo "$(basename $i) is not finished"
+				fi
+			done
+			exit 1
+		fi
+	done
+	echo ""
+	echo "${_projectName} test succeeded!"
+	echo ""
+}
+
 # ========================
 # ARG PARSING
 # ========================
@@ -69,7 +100,7 @@ while [[ $# -gt 0 ]]; do
       WORKFLOW="${2:-}"
       shift 2
       ;;
-    -p|--pipeline)
+     )
       PIPELINE="${2:-}"
       shift 2
       ;;
@@ -88,19 +119,15 @@ while [[ $# -gt 0 ]]; do
 done
 
 # ========================
-# VALIDATION
+# CHECK PARAMS
 # ========================
 [[ -z "${SAMPLESHEET}" ]] && die "Missing --samplesheet"
 [[ -z "${WORKFLOW}" ]] && die "Missing --workflow"
 [[ -z "${WORKDIR}" ]] && die "Missing --workdir"
 [[ -z "${PIPELINE}" ]] && die "Missing --pipeline"
-
 [[ -f "${PIPELINE}" ]] || die "Pipeline not found: ${PIPELINE}"
 [[ -f "${WORKFLOW}" ]] || die "Workflow not found: ${WORKFLOW}"
 
-# ========================
-# CONTEXT INFO
-# ========================
 log "Starting pipeline"
 log "Samplesheet : ${SAMPLESHEET}"
 log "Workflow    : ${WORKFLOW}"
@@ -110,99 +137,58 @@ log "SLURM job   : ${SLURM_JOB_ID:-local}"
 
 START_TIME=$(date +%s)
 
-# ========================
-# WAIT until FINISHED
-# ========================
-function checkIfFinished(){
-	local _projectName="${1}"
-	count=0
-	minutes=0
-	while [ ! -f "/groups/umcg-atd/tmp08/projects/NGS_RNA/${_projectName}/run01/jobs/*CopyToResultsDir_0.sh.sh.finished" ]
-	do
-
-		echo "${_projectName} is not finished in $minutes minutes, sleeping for 2 minutes"
-		sleep 120
-		minutes=$((minutes+2))
-
-		count=$((count+2))
-		if [[ "${count}" -eq 35 ]]
-		then
-			echo "the test was not finished within 35 minutes, let's kill it"
-			echo -e "\n"
-			for i in "/groups/umcg-atd/tmp08/projects/NGS_RNA/${_projectName}/run01/jobs/"*".sh"
-			do
-				if [[ ! -f "${i}.finished" ]]
-				then
-					echo "$(basename $i) is not finished"
-				fi
-			done
-			exit 1
-		fi
-	done
-	echo ""
-	echo "${_projectName} test succeeded!"
-	echo ""
-}
 
 # ========================
-# RUN PIPELINE
+# PREPARE AND RUN PIPELINE
 # ========================
 
-	_projectName=$(basename "${WORKDIR}")
-  _sheetName=$(basename "${SAMPLESHEET%.csv}")
-	_generatedScriptsFolder="${WORKDIR}/generatedscripts"
+_projectName=$(basename "${WORKDIR}")
+_sheetName=$(basename "${SAMPLESHEET%.csv}")
+_generatedScriptsFolder="${WORKDIR}/generatedscripts"
 
-	#cleanup
-  rm -rf "/groups/umcg-atd/tmp08/projects/NGS_RNA/${_projectName}"
-	mkdir -p  "${WORKDIR}"/{tmp,generatedscripts,projects}
-  mkdir -p  "${WORKDIR}/rawdata/ngs/MY_TEST_BAM_PROJECT"
+#cleanup
+rm -rf "/groups/umcg-atd/${TMP}/projects/NGS_RNA/${_projectName}"
+mkdir -p  "${WORKDIR}"/{tmp,generatedscripts,projects}
+mkdir -p  "${WORKDIR}/rawdata/ngs/MY_TEST_BAM_PROJECT"
 
-  rsync -r --verbose --recursive --links --no-perms --times --group --no-owner --devices --specials "${PIPELINE}/test/rawdata/MY_TEST_BAM_PROJECT/"SRR1552906[249]_[12].fq.gz "${WORKDIR}/rawdata/ngs/MY_TEST_BAM_PROJECT/"
-
-
-	echo "copy generate template"
-	cp "${PIPELINE}/templates/generate_template.sh" "${_generatedScriptsFolder}/generate_template.sh"
+rsync -r --verbose --recursive --links --no-perms --times --group --no-owner --devices --specials "${PIPELINE}/test/rawdata/MY_TEST_BAM_PROJECT/"SRR1552906[249]_[12].fq.gz "${WORKDIR}/rawdata/ngs/MY_TEST_BAM_PROJECT/"
 
 
-	module load NGS_RNA/betaAutotest
-	module list
+echo "copy generate template"
+cp "${PIPELINE}/templates/generate_template.sh" "${_generatedScriptsFolder}/generate_template.sh"
 
-	#perl -pi -e 's|WORKFLOW=\${EBROOTNGS_RNA}/workflow_\${PIPELINE}.csv|WORKFLOW=\${EBROOTNGS_RNA}/test_workflow_\${PIPELINE}.csv|' "${_generatedScriptsFolder}/generate_template.sh"
 
-	cp "${PIPELINE}/test/samplesheets/${SAMPLESHEET}" "${_generatedScriptsFolder}"
+module load NGS_RNA/betaAutotest
+module list
 
-	perl -pi -e "s|/groups/umcg-atd/tmp01/|/groups/umcg-atd/${TMP}/|g" "${_generatedScriptsFolder}/${SAMPLESHEET}"
-  perl -p -e "s|${_sheetName}|${_projectName}|g" "${_generatedScriptsFolder}/${SAMPLESHEET}" > "${_generatedScriptsFolder}/${SAMPLESHEET}.tmp"
+#fix template and samplesheet for test
+cp "${PIPELINE}/test/samplesheets/${SAMPLESHEET}" "${_generatedScriptsFolder}"
+perl -pi -e "s|/groups/umcg-atd/tmp01/|/groups/umcg-atd/${TMP}/|g" "${_generatedScriptsFolder}/${SAMPLESHEET}"
+perl -p -e "s|${_sheetName}|${_projectName}|g" "${_generatedScriptsFolder}/${SAMPLESHEET}" > "${_generatedScriptsFolder}/${SAMPLESHEET}.tmp"
+mv -v "${_generatedScriptsFolder}/${SAMPLESHEET}"{.tmp,}
+cd "${_generatedScriptsFolder}/"
+perl -pi -e 's|parameters.\${host}|parameters.talos|g' generate_template.sh
   
-	mv -v "${_generatedScriptsFolder}/${SAMPLESHEET}"{.tmp,}
+bash generate_template.sh -c "${EBROOTNGS_RNA}/create_external_samples_ngs_projects_workflow.csv" -g umcg-atd -p "${SAMPLESHEET%.csv}" -w "${_generatedScriptsFolder}" -f "${SAMPLESHEET%.csv}" -t "${TMP}"
+	
+cd scripts
+	
+bash CreateExternSamplesProjects_0.sh
+	
+cd "/groups/umcg-atd/tmp08/projects/NGS_RNA/${_projectName}/run01/jobs/"
+  
+# reduce runtime 
+perl -pi -e 's|-ERC GVCF|-L 1:17383226-183837051 \\\n  -ERC GVCF|' s*_GatkHaplotypeCallerGvcf_*.sh
+perl -pi -e 's|-ERC GVCF|-L 1:17383226-183837051 \\\n  -ERC GVCF|' s*_GatkGenotypeGvcf_*.sh
+perl -pi -e 's|rsync -av .*.vip|#rsync -av .*.vip|g' s*_CopyToResultsDir_*.sh
+perl -pi -e 's|mem=40gb|mem=10gb|' *.sh
+perl -pi -e 's|--time=16:00:00|--time=05:59:00|' *.sh
+perl -pi -e 's|--time=23:00:00|--time=05:59:00|' *.sh
+perl -pi -e 's|--time=23:59:00|--time=05:59:00|' *.sh
 
-	cd "${_generatedScriptsFolder}/"
+sh submit.sh
 
-  perl -pi -e 's|parameters.\${host}|parameters.talos|g' generate_template.sh
-
-  bash generate_template.sh -c "${EBROOTNGS_RNA}/create_external_samples_ngs_projects_workflow.csv" -g umcg-atd -p "${SAMPLESHEET%.csv}" -w "${_generatedScriptsFolder}" -f "${SAMPLESHEET%.csv}" -t "${TMP}"
-
-#	bash generate_template.sh -c "${EBROOTNGS_RNA}/create_external_samples_ngs_projects_workflow.csv"
-	cd scripts
-
-	#perl -pi -e "s|workflow_GD.csv|test_workflow_GD.csv|" *.sh
-
-	bash CreateExternSamplesProjects_0.sh
-
-	cd "/groups/umcg-atd/tmp08/projects/NGS_RNA/${_projectName}/run01/jobs/"
-
-	pwd
-
-	perl -pi -e 's|-ERC GVCF|-L 1:17383226-183837051 \\\n  -ERC GVCF|' s*_GatkHaplotypeCallerGvcf_*.sh
-	perl -pi -e 's|-ERC GVCF|-L 1:17383226-183837051 \\\n  -ERC GVCF|' s*_GatkGenotypeGvcf_*.sh
-	perl -pi -e 's|rsync -av .*.vip|#rsync -av .*.vip|g' s*_CopyToResultsDir_*.sh
-	perl -pi -e 's|mem=40gb|mem=10gb|' *.sh
-	perl -pi -e 's|--time=16:00:00|--time=05:59:00|' *.sh
-	perl -pi -e 's|--time=23:00:00|--time=05:59:00|' *.sh
-	perl -pi -e 's|--time=23:59:00|--time=05:59:00|' *.sh
-
-	sh submit.sh
-
-  checkIfFinished "${_projectName}"
+# wait until pipeline finishes
+checkIfFinished "${_projectName}"
 
 exit 0
